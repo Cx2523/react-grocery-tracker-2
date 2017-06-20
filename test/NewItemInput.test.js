bc7fd9bea0cc6b7c4e0cd8925c48a8ea34218bce
Christopher Caldwell <clc523@gmail.com>
2017-06-19 18:36:16 -0600

    initialized test suite for NewItemInput component

---
+++ test/NewItemInput.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NewItemInput from '../src/react-components/NewItemInput.js';
+import { expect } from 'chai';
+
+describe('NewItemInput', function(){
+  it('should intialize as hidden', function(){});
+  it('should contain 3 input boxes', function(){});
+  it('should contain a submit button', function(){});
+  it('should contain a clear button', function(){});
+  it('local state should clear on submit', function(){});
+  it('local state should clear on clear', function(){});
+});
