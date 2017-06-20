8e3e9c23ed072eda1744013df035df1935ec3690
Christopher Caldwell <clc523@gmail.com>
2017-06-19 19:11:31 -0600

    added failing enzyme tests for NewItemInput

--- test/NewItemInput.test.js
+++ test/NewItemInput.test.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import NewItemInput from '../src/react-components/NewItemInput.js';
+import NewItemInput from '../src/react-components/Items/NewItemInput.js';
 import { expect } from 'chai';
 
 describe('NewItemInput', function(){
-  it('should intialize as hidden', function(){});
-  it('should contain 3 input boxes', function(){});
-  it('should contain a submit button', function(){});
-  it('should contain a clear button', function(){});
-  it('local state should clear on submit', function(){});
-  it('local state should clear on clear', function(){});
+  const wrapper = shallow(<NewItemInput />);
+  it('should intialize as hidden', function(){
+    expect(wrapper.state().hidden).to.be.true;
+  });
+  it('should contain 3 input boxes', function(){
+    expect(wrapper.find('Input').length).to.be(3);
+  });
+  it('should contain a submit button', function(){
+    expect(wrapper.find('.newItemSubmit').length).to.be(1);
+  });
+  it('should contain a clear button', function(){
+    expect(wrapper.find('.newItemClear').length).to.be(1);
+  });
+  it('local state should clear on submit');
+  it('local state should clear on clear');
 });
