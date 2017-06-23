fe14490e90319639fb80b20221f4d3fbe52a4059
Christopher Caldwell <clc523@gmail.com>
2017-06-23 16:53:50 -0600

    changed Item to a class component, so that it can have a local state which manages formatting changes

--- src/react-components/Items/Item.js
+++ src/react-components/Items/Item.js
@@ -1,29 +1,46 @@
 import React from 'react';
 import { Segment, Icon } from 'semantic-ui-react';
 
-const Item = (props) => {
-  const getItemById = () => {
-    props.getItemById(props.item.id);
+class Item extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      format: '',
+      iconAnimate: '',
+      iconSize: 'big'
+    }
+    this.getItemById = this.getItemById.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
-  const deleteItem = () => {
-    props.deleteItem(props.item.id);
+  getItemById(){
+    this.setState({
+      format: "inverted yellow",
+      iconAnimate: 'loading',
+      iconSize: 'huge'
+    });
+    this.props.getItemById(this.props.item.id);
+  }
+  deleteItem(){
+    this.props.deleteItem(this.props.item.id);
+  }
+  render(){
+    return (
+      <Segment className={this.state.format} raised>
+        <div className="item-card">
+          <div className="item-info">
+            <h1>{this.props.item.name}</h1>
+            <h1>{this.props.item.cost}</h1>
+          </div>
+          <div className="item-control">
+            <Icon color="green" size="big" name="plus" />
+            <Icon className={this.state.iconAnimate} onClick={this.getItemById} size={this.state.iconSize} name="setting" />
+            <Icon onClick={this.deleteItem} color="red" size="big" name="remove" />
+          </div>
+        </div>
+      </Segment>
+    );
   }
 
-  return (
-    <Segment raised>
-      <div className="item-card">
-        <div className="item-info">
-          <h1>{props.item.name}</h1>
-          <h1>{props.item.cost}</h1>
-        </div>
-        <div className="item-control">
-          <Icon color="green" size="big" name="plus" />
-          <Icon onClick={getItemById} size="big" name="setting" />
-          <Icon onClick={deleteItem} color="red" size="big" name="remove" />
-        </div>
-      </div>
-    </Segment>
-  );
 }
 
 export default Item;
